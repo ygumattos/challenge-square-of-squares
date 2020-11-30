6e8b0c2be3b8160428275cf4e6151eb10bfbbc46
Ygor Mattos <ygormattos.b@gmail.com>
2020-11-30 17:46:00 -0300

    :hammer: add timestamps in BD

--- src/app/models/Territory.js
+++ src/app/models/Territory.js
@@ -1,36 +1,44 @@
 import mongoose from 'mongoose';
 
-const TerritorySchema = new mongoose.Schema({
-  id: {
-    type: Number,
+const TerritorySchema = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    start: {
+      type: Object,
+      required: true,
+    },
+    end: {
+      type: Object,
+      required: true,
+    },
+    location: {
+      type: Object,
+      required: true,
+    },
+    area: {
+      type: Number,
+      required: true,
+    },
+    painted_area: {
+      type: Number,
+      required: true,
+    },
+    painted_squares: {
+      type: Array
+    },
   },
-  name: {
-    type: String,
-    required: true,
-  },
-  start: {
-    type: Object,
-    required: true,
-  },
-  end: {
-    type: Object,
-    required: true,
-  },
-  location: {
-    type: Object,
-    required: true,
-  },
-  area: {
-    type: Number,
-    required: true,
-  },
-  painted_area: {
-    type: Number,
-    required: true,
-  },
-  painted_squares: {
-    type: Array
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
   }
-});
+);
 
 export default mongoose.model('Territory', TerritorySchema);
